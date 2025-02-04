84e7c5ea8d9d3d8278fa4ed9f1bb8eb8c4963b5d
ryan nemeth <ryanthomasnemeth@gmail.com>
2025-02-03 20:49:22 -0500

    switching to cobra

---
+++ cmd/root.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2025 Ryan Nemeth
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/rnemeth90/yahba/internal/config"
+	"github.com/spf13/cobra"
+)
+
+var c config.Config
+
+// Root command
+var rootCmd = &cobra.Command{
+	Use:   "yahba",
+	Short: "YAHBA is a high-performance HTTP load testing tool",
+	Long: `Yet Another HTTP Benchmark Application (YAHBA)
+A fast, customizable HTTP load tester.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Error: No command specified. Use 'yahba test' or 'yahba --help' for usage information.")
+		os.Exit(1)
+	},
+}
+
+// Execute runs the root command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Add subcommands
+	rootCmd.AddCommand(runCmd)
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.23.0
 
 require (
 	github.com/fatih/color v1.18.0 // indirect
+	github.com/go-critic/go-critic v0.11.5 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/quasilyte/go-ruleguard/dsl v0.3.22 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
