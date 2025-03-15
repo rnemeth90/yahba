5c6dce7afd0bdf6548fa0431c41271513aa6850e
ryan nemeth <ryanthomasnemeth@gmail.com>
2025-03-14 20:35:05 -0400

    test: testcreaterequest

--- internal/worker/worker_test.go
+++ internal/worker/worker_test.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/rnemeth90/yahba/internal/config"
+	"github.com/stretchr/testify/assert"
 )
 
 // Mock server to simulate HTTP requests
@@ -15,5 +18,32 @@ func mockServer() *httptest.Server {
 }
 
 func TestNewWorker(t *testing.T) {
-	// just test the worker ID
+	w := NewWorker(1, nil, nil, nil, config.Config{})
+	if w == nil {
+		t.Error("Expected worker to not be nil")
+	}
+
+	// Check worker ID
+	if w.ID != 1 {
+		t.Errorf("Expected worker ID to be 1, got %d", w.ID)
+	}
+}
+
+func TestCreateRequest(t *testing.T) {
+	mockConfig := config.Config{}
+	mockClient := http.Client{}
+	worker := NewWorker(1, nil, nil, &mockClient, mockConfig)
+
+	job := Job{
+		ID:     1,
+		Host:   "http://example.com",
+		Method: "GET",
+		Body:   "",
+	}
+
+	req, err := worker.createRequest(job)
+
+	assert.NoError(t, err)
+	assert.Equal(t, req.Method, "GET")
+	assert.Equal(t, req.URL.String(), "http://example.com")
 }
